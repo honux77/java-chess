47b876327a4fa6836f6962eb0a5f277294d20433
Hoyoung Jung <honux77@gmail.com>
2021-01-19 19:46:27 +0900

    Add black Pawn instance
    검정 폰 생성

--- src/test/java/net/honux/chess/PawnTest.java
+++ src/test/java/net/honux/chess/PawnTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class PawnTest {
+
     @Test
     @DisplayName("흑백 객체가 잘 생성되는지 확인")
     void create() {
-        Pawn p = new Pawn("white");
-        assertThat(p).isNotNull();
+        String white = "white";
+        String black = "black";
+        Pawn p1 = new Pawn(white);
+        Pawn p2 = new Pawn(black);
+
+        assertThat(p1.getColor()).isEqualTo(white);
+        assertThat(p2.getColor()).isEqualTo(black);
+
     }
 }
